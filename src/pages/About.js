76dadb12fccb8cad5d8e6fc167fc1d0eda29f588
lhsthree <lhsthree@gmail.com>
2021-02-27 09:58:52 -0600

    feat: set up links to about and home and set up error page

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,10 +2,12 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
+    <section className="section about-section">
+    	<h1 className="section-title">about us</h1>
+    	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum et nulla massa. Nunc commodo interdum feugiat. In tellus massa, condimentum id sagittis a, ultricies id odio. Nam rutrum mollis velit, a vehicula nunc. In placerat ante quis interdum pharetra. Duis elit lectus, varius vitae congue quis, tincidunt eu orci. Maecenas tincidunt, lacus rutrum vulputate tincidunt, quam mauris lobortis eros, a elementum mauris ipsum vitae massa. Curabitur elementum velit sem, ac sodales est venenatis a. Morbi fringilla cursus metus, sed aliquet odio venenatis nec. Fusce elementum dictum condimentum. Aliquam id condimentum ligula. Mauris finibus ligula quis sapien ultrices, non laoreet purus sodales. Sed vitae ante et libero condimentum ultricies.
+    	</p>
+    </section>
+    )
 }
 
 export default About
